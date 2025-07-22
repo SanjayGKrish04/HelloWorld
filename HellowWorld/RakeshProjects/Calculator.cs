9c1fcd028d2a3f6126b72c09f371efc974d759f9
Rakesh <rakeshgkrriz@gmail.com>
2025-07-22 13:33:08 +0530

    calculator added

---
+++ HellowWorld/RakeshProjects/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            Console.Write("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            char op = char.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"Result: {num1} - {num2} = {result}");
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"Result: {num1} * {num2} = {result}");
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"Result: {num1} / {num2} = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+            }
+        }
+    }
+}
